f843c966cfa96176564679a1657683c38fbcea5c
Melanie Burger <melanie.brgr@gmail.com>
2015-12-10 00:09:18 -0500

    Escape and close modal functionality added

--- js/aboutme.js
+++ js/aboutme.js
@@ -5,37 +5,54 @@ $(document).ready(function() {
 		e.stopPropagation();
 		return false;
 	});
+	$('input#company').focus();
 
 	$('input#company').keyup(function(e){
 		var company = $(this).val();
 
+		//If enter is pressed from input field, do this
 		if(e.keyCode === 13) {
     		$('.modal').load('../modal.html', function() {
-				$('.modal h2').text('Thanks for hiring me at ' + company + '!');
 
-    			$("#submit-email").click(function() {
-    				$('#company-email-form').submit();
-    			});
+    			//If something was entered in the company form field, update h2 to include it
+				if (company) {
+					$('.modal h2').text('Thanks for hiring me at ' + company + '!');
+				} else {
+					$('.modal h2').text('Thanks for hiring me!');
+				}
 
-    			$('#start-date-form').submit(function(e){
-					e.preventDefault();
-					e.stopPropagation();
-					return false;
-				});
+				$("input#start-date").focus(); // otherwise Esc keyup doesn't work??
 
-				$('#company-email-form').submit(function(e){
+				// On start-date-form submit or submit-email button click, do this
+				$('#start-date-form').submit(function(e){
 					e.preventDefault();
 					e.stopPropagation();
 
 					var startDate = $('#start-date').val();
-					//var companyEmail = $('#company-email').val();
-					var emailString = "mailto:melanie.brgr@gmail.com?subject=email from " + company + "&body=Hi, you will start at " + company + " on " + startDate;
+					var emailString = "mailto:melanie.brgr@gmail.com?subject=email from " + company + "&body=Hi, you will start at " + company + " on " + startDate +".";
 					$("#send-email").attr("href", emailString);
 					$("#send-email")[0].click(); //[0] to target href
 
 					return false; 
 				});
+				$("#submit-email").click(function() {
+    				$('#start-date-form').submit();
+    			});
 
+				//Close modal with x button or by pressing Esc
+				$('.close-modal').click(function() {
+					$('.close-modal').parents('div.modal').empty();
+					$('.modal').css('display','none');
+				});
+				function escKeyUp(e) {
+					if (e.keyCode === 27) {
+				  		$('.close-modal').click();
+				  		$(document).unbind("keyup", escKeyUp);
+				  	}
+					return false;
+				}
+				$(document).keyup(escKeyUp);					
+					
     		});
 			$('.modal').css('display','block');
 		}
