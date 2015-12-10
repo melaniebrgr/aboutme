db8d7ae2c3e0b68a94d1cf74c15e7f96f2665982
Melanie Burger <melanie.brgr@gmail.com>
2015-12-09 21:48:42 -0500

    Send email on form feature added

--- js/aboutme.js
+++ js/aboutme.js
@@ -7,9 +7,36 @@ $(document).ready(function() {
 	});
 
 	$('input#company').keyup(function(e){
-		if(e.keyCode === 13) //return key code
-		{
-    		$('.modal').load('../modal.html');
+		var company = $(this).val();
+
+		if(e.keyCode === 13) {
+    		$('.modal').load('../modal.html', function() {
+				$('.modal h2').text('Thanks for hiring me at ' + company + '!');
+
+    			$("#submit-email").click(function() {
+    				$('#company-email-form').submit();
+    			});
+
+    			$('#start-date-form').submit(function(e){
+					e.preventDefault();
+					e.stopPropagation();
+					return false;
+				});
+
+				$('#company-email-form').submit(function(e){
+					e.preventDefault();
+					e.stopPropagation();
+
+					var startDate = $('#start-date').val();
+					//var companyEmail = $('#company-email').val();
+					var emailString = "mailto:melanie.brgr@gmail.com?subject=email from " + company + "&body=Hi, you will start at " + company + " on " + startDate;
+					$("#send-email").attr("href", emailString);
+					$("#send-email")[0].click(); //[0] to target href
+
+					return false; 
+				});
+
+    		});
 			$('.modal').css('display','block');
 		}
 		return false;
